f0e227d8d759809a40f8274a88ec7c4cb1b724f5
JIHONG KIM <64270116+ghk0409@users.noreply.github.com>
2023-06-11 14:54:50 +0900

    Test: GithubAction 연동 테스트

--- src/users/users.resolver.ts
+++ src/users/users.resolver.ts
@@ -7,4 +7,9 @@ export class UsersResolver {
         console.log('test');
         return '테스트 중입니당';
     }
+
+    @Query((returns) => String)
+    test2(): string {
+        return '깃헙 액션 연동 성공!!!!';
+    }
 }
